d86b73582e71b27eca370f2e3bb1013c343c1002
solbencas <cecibenitezca@gmail.com>
2020-09-02 11:56:27 -0400

    Show instructions:
    This method is showing the instructions for tic-tac-toe.

--- bin/main.rb
+++ bin/main.rb
@@ -2,3 +2,10 @@
 
 $player_x = ''
 $player_o = ''
+
+def instructions
+  puts "\nWelcome to Tic Tac Toe Game!\n"\
+  "\n 1 | 2 | 3 \n-----------\n 4 | 5 | 6 \n-----------\n 7 | 8 | 9 \n"\
+  "\nJust input your names and pick a number to place your mark"\
+  ' until one of you TIC TAC TOE!!!'
+end
