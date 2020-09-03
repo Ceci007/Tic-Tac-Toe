31c1e1539894ef54b2833be1d37c5211aa066390
solbencas <cecibenitezca@gmail.com>
2020-09-03 13:11:37 -0400

    Player positions:
    Attribute positions hash created to associate the users input with the positions on the board.

--- lib/player.rb
+++ lib/player.rb
@@ -3,5 +3,8 @@ class Player
   def initialize(name, mark)
     @name = name
     @mark = mark
+    @positions = { 1 => [0, 0], 2 => [0, 1], 3 => [0, 2],
+                   4 => [1, 0], 5 => [1, 1], 6 => [1, 2],
+                   7 => [2, 0], 8 => [2, 1], 9 => [2, 2] }
   end
 end
