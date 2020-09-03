2e915c0677bf9a3ee177954facec39f7e066575d
John Elvis Durán <jelduran@gmail.com>
2020-09-03 12:16:33 -0500

    Player make a move:
    Method make_move created to translate the player input to coordinates in the board, and use the board's method move, to make a move.

--- lib/player.rb
+++ lib/player.rb
@@ -7,4 +7,9 @@ class Player
                    4 => [1, 0], 5 => [1, 1], 6 => [1, 2],
                    7 => [2, 0], 8 => [2, 1], 9 => [2, 2] }
   end
+
+  def make_move(position, board)
+    xy_pos = @positions[position]
+    board.move(xy_pos[0], xy_pos[1], @mark)
+  end
 end
