999d1a971366db947676f3773bce764a1a796e12
John Elvis Durán <jelduran@gmail.com>
2020-09-03 10:54:58 -0500

    Show board:
    Method show_board created to display the board on terminal.

--- lib/board.rb
+++ lib/board.rb
@@ -5,4 +5,12 @@ class Board
     @move_number = 0
     @winner = false
   end
+
+  def show_board
+    puts "\n #{@positions[0][0]} | #{@positions[0][1]} | #{@positions[0][2]} \n"\
+     "----------- \n"\
+     " #{@positions[1][0]} | #{@positions[1][1]} | #{@positions[1][2]} \n"\
+     "----------- \n"\
+     " #{@positions[2][0]} | #{@positions[2][1]} | #{@positions[2][2]} \n"
+  end
 end
