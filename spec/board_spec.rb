cbdb8d983021d105c23decd1d6cef4bc9a1235ee
solbencas <cecibenitezca@gmail.com>
2020-09-08 15:37:16 -0400

    Add #initialize:
    We Add the #initialize with some tests.

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,12 +1,18 @@
-# require_relative '../lib/player'
-# require_relative '../lib/board'
+require_relative '../lib/player'
+require_relative '../lib/board'
 
-# describe do Board 
-  #let(:player_x) { Player.new('Ceci', 'X') }
-  #let(:player_o) { Player.new('Marcos', 'O') }
-  #let(:board) { Board.new } 
+describe do Board 
+  let(:player_x) { Player.new('Ceci', 'X') }
+  let(:player_o) { Player.new('Marcos', 'O') }
+  let(:board) { Board.new } 
 
-  #describe "#initialize" do 
+  describe "#initialize" do 
+    it "Creates a board instance" do 
+      expect(board).to be_an_instance_of Board
+    end 
 
-  #end
-#end
+    it "Reads the instance variable @winner successfully" do 
+      expect(board.winner).to eql(false)
+    end
+  end
+end
