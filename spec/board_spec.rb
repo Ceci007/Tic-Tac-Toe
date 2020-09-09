1a7aeceaacfd6d75ea18ace716022e3ec1ceb999
Marcos Hernández Campos <marcoshdezcam@gmail.com>
2020-09-09 10:38:40 -0500

    Fix all Rubocop offenses and add coverage/ to .gitignore
    Change double quoted and single quoted strings for %()
    Delete trailing whitespaces
    Fix indentation

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,89 +1,89 @@
 require_relative '../lib/player'
 require_relative '../lib/board'
 
-describe do Board 
+describe Board do
   let(:player_x) { Player.new('Ceci', 'X') }
   let(:player_o) { Player.new('Marcos', 'O') }
-  let(:board) { Board.new } 
+  let(:board) { Board.new }
 
-  describe "#initialize" do 
-    it "Creates a board instance" do 
+  describe %(#initialize) do
+    it %(Creates a board instance) do
       expect(board).to be_an_instance_of Board
-    end 
+    end
 
-    it "Reads the instance variable @winner successfully" do 
+    it %(Reads the instance variable @winner successfully) do
       expect(board.winner).to eql(false)
     end
   end
 
-  describe "#current_board" do
-    it "returns the board slots empty" do
+  describe %(#current_board) do
+    it %(returns the board slots empty) do
       expect(board.current_board).to eql("\n   |   |   \n----------- \n   |   |   \n----------- \n   |   |   \n")
     end
   end
 
-  describe "#move" do
-    it "marks pos[1][1] with an X on the board" do
+  describe %(#move) do
+    it %(marks pos[1][1] with an X on the board) do
       board.move(1, 1, 'X')
       expect(board.current_board).to eql("\n   |   |   \n----------- \n   | X |   \n----------- \n   |   |   \n")
     end
 
-    it "marks pos[0][2] with an O on the board" do 
+    it %(marks pos[0][2] with an O on the board) do
       board.move(0, 2, 'O')
       expect(board.current_board).to eql("\n   |   | O \n----------- \n   |   |   \n----------- \n   |   |   \n")
     end
-  end 
+  end
 
-  describe "#check_winner" do 
-    it "Checks if there is a winner on the row" do 
+  describe %(#check_winner) do
+    it %(Checks if there is a winner on the row) do
       board.move(0, 0, 'X')
       board.move(0, 1, 'X')
       board.move(0, 2, 'X')
       expect(board.check_winner(0, 1, 'X')).not_to eql(false)
     end
 
-    it "Checks if there is a winner on the col 0" do
+    it %(Checks if there is a winner on the col 0) do
       board.move(0, 1, 'O')
       board.move(1, 1, 'O')
       board.move(2, 1, 'O')
       expect(board.check_winner(0, 1, 'O')).not_to eql(false)
     end
 
-    it "Checks if there is a winner on the first diagonal" do
+    it %(Checks if there is a winner on the first diagonal) do
       board.move(0, 0, 'O')
       board.move(1, 1, 'O')
       board.move(2, 2, 'O')
       expect(board.check_winner(0, 0, 'O')).not_to eql(false)
     end
 
-    it "Checks if there is a winner on the second diagonal" do
+    it %(Checks if there is a winner on the second diagonal) do
       board.move(0, 2, 'X')
       board.move(1, 1, 'X')
       board.move(2, 0, 'X')
       expect(board.check_winner(0, 2, 'X')).not_to eql(false)
     end
-  end 
+  end
 
-  describe "#reset" do 
-    it "Reset the psoitions array" do 
+  describe %(#reset) do
+    it %(Reset the psoitions array) do
       board.reset
       expect(board.positions).to eql([[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']])
-    end 
+    end
 
-    it "Reset the move number to zero" do 
+    it %(Reset the move number to zero) do
       board.reset
       expect(board.move_number).to eql(0)
     end
 
-    it "Reset the winner to false" do 
+    it %(Reset the winner to false) do
       board.reset
       expect(board.winner).to eql(false)
     end
-  end 
+  end
 
-  describe "#empty_position?" do 
-    it "returns true if the position is empty" do
-      board.move(0, 0, ' ') 
+  describe %(#empty_position?) do
+    it %(returns true if the position is empty) do
+      board.move(0, 0, ' ')
       xy_pos = 1
       expect(board.empty_position?(xy_pos[0], xy_pos[1])).to eql(true)
     end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,40 +1,40 @@
 require_relative '../lib/player'
 require_relative '../lib/board'
 
-describe Player do 
-  describe '#initialize' do 
+describe Player do
+  describe '#initialize' do
     let(:player_x) { Player.new('Ceci', 'X') }
     let(:player_o) { Player.new('Marcos', 'O') }
-    let(:board) { Board.new } 
+    let(:board) { Board.new }
 
-    describe '#initialize' do 
-      it "Creates a player_x instance" do 
+    describe %(#initialize) do
+      it %(Creates a player_x instance) do
         expect(player_x).to be_an_instance_of Player
-      end 
+      end
 
-      it "Creates a player_o instance" do 
+      it %(Creates a player_o instance) do
         expect(player_o).to be_an_instance_of Player
       end
     end
 
-    it "returns the player's name" do 
+    it %(returns the player's name) do
       expect(player_x.name).to eql('Ceci')
-    end 
+    end
 
-    it "retuns true if the position is not yet taken by player_x" do 
+    it %(retuns true if the position is not yet taken by player_x) do
       expect(player_x.check_position?(1, board)).to eql(true)
-    end 
+    end
 
-    it "converts the hask key into x and y coordinates and returns the position on the board" do 
+    it %(converts the hask key into x and y coordinates and returns the position on the board) do
       xy_pos = 1
       expect(player_x.make_move(1, board)).to eql(board.move(xy_pos[0], xy_pos[1], 'X'))
     end
 
-    it "return the other player's name" do
+    it %(return the other player's name) do
       expect(player_o.name).to eql('Marcos')
     end
 
-    it "returns true if the position is not yet taken by player_o" do
+    it %(returns true if the position is not yet taken by player_o) do
       expect(player_o.check_position?(3, board)).to eql(true)
     end
   end
